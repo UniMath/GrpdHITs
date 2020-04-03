41181c8630a0733fec4d4ba9433f9365c204f049
Niels <nnmvdw@gmail.com>
2020-04-03 23:09:03 +0200

    UMP of coequifier

--- code/examples/coequifier.v
+++ code/examples/coequifier.v
@@ -4,11 +4,13 @@ Require Import UniMath.MoreFoundations.All.
 
 Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.Bicategories.Core.Bicat.
+Import Bicat.Notations.
 Require Import UniMath.Bicategories.Core.Examples.OneTypes.
 Require Import UniMath.Bicategories.DisplayedBicats.DispBicat.
 Require Import UniMath.Bicategories.DisplayedBicats.Examples.Algebras.
 Require Import UniMath.Bicategories.DisplayedBicats.Examples.DispDepProd.
 Require Import UniMath.Bicategories.DisplayedBicats.Examples.Add2Cell.
+Require Import UniMath.Bicategories.Colimits.Initial.
 
 Require Import prelude.all.
 Require Import signature.hit_signature.
@@ -19,6 +21,8 @@ Require Import algebra.one_types_homotopies.
 Require Import displayed_algebras.globe_over_lem.
 Require Import displayed_algebras.displayed_algebra.
 Require Import displayed_algebras.total_algebra_map.
+Require Import existence.hit_existence.
+Require Import existence.initial_algebra.
 
 Local Open Scope cat.
 
@@ -273,7 +277,7 @@ Section CoequifierSignature.
     simpl.
     unfold s_endpoint.
     unfold t_endpoint.
-    apply ap_constr.
+    apply ap_e.
     apply fmap_eq.
     apply funextsec.
     exact p.
@@ -291,7 +295,7 @@ Section CoequifierSignature.
     simpl.
     unfold s_endpoint.
     unfold t_endpoint.
-    apply ap_constr.
+    apply ap_e.
     apply fmap_eq.
     apply funextsec.
     exact q.
@@ -307,18 +311,19 @@ Section CoequifierSignature.
     - exact no_endpoint.
     - exact no_endpoint.
     - exact unit.
-    - intro. exact (C A).
-    - intro. exact (C unit_one_type).
-    - intro. exact (c (C A) (tt : unit_one_type)).
-    - intro. exact (c (C A) (tt : unit_one_type)).
+    - exact (λ _, C A).
+    - exact (λ _, C unit_one_type).
+    - exact (λ _, c (C A) (tt : unit_one_type)).
+    - exact (λ _, c (C A) (tt : unit_one_type)).
     - intro. exact s_endpoint.
     - intro. exact t_endpoint.
     - intro. exact left_homot_endpoint.
     - intro. exact right_homot_endpoint.
   Defined.
 
+  (** Projections and builders *)
   Section CoequifierProjections.
-    Context {X : hit_algebra_one_types coequifier_signature}.
+    Context (X : hit_algebra_one_types coequifier_signature).
 
     Definition coequif_inc
       : B → alg_carrier X
@@ -326,34 +331,131 @@ Section CoequifierSignature.
 
     Definition coequif_homot
                (x : A)
-      : maponpaths
-          coequif_inc
-          (p x)
+      : maponpaths coequif_inc (p x)
         =
-        maponpaths
-          coequif_inc
-          (q x).
+        maponpaths coequif_inc (q x).
     Proof.
-      pose (alg_homot X tt x (idpath _)) as i.
-      simpl in i.
-      cbn in i.
-      refine (_ @ i @ _).
-      - apply maponpaths.
-        refine (!_).
-        apply sem_fmap_funextsec_eq.
-      - apply maponpaths.
-        apply sem_fmap_funextsec_eq.
+      exact (maponpaths (maponpaths coequif_inc) (!(sem_fmap_funextsec_eq _ _ _))
+             @ alg_homot X tt x (idpath _)
+             @ maponpaths (maponpaths coequif_inc) ((sem_fmap_funextsec_eq _ _ _))).
     Qed.
   End CoequifierProjections.
 
+  Section MakeCoequifierAlgebra.
+    Context {X : one_type}
+            (inc : B → X)
+            (homot : ∏ (x : A),
+                     maponpaths inc (p x)
+                     =
+                     maponpaths inc (q x)).
+
+    Local Definition make_coequifier_path_algebra
+      : hit_path_algebra_one_types coequifier_signature.
+    Proof.
+      use make_hit_path_algebra.
+      - use make_hit_prealgebra.
+        + exact X.
+        + apply one_type_isofhlevel.
+        + exact inc.
+      - intro j ; induction j.
+    Defined.
+
+    Local Definition make_coequifier_is_algebra
+      : is_hit_algebra_one_types coequifier_signature make_coequifier_path_algebra.
+    Proof.
+      intros j x r ; simpl in x.
+      exact (maponpaths (maponpaths inc) (sem_fmap_funextsec_eq _ _ _)
+             @ homot x
+             @ maponpaths (maponpaths inc) (!(sem_fmap_funextsec_eq _ _ _))).
+    Qed.
+    
+    Definition make_coequifier_algebra
+      : hit_algebra_one_types coequifier_signature.
+    Proof.
+      use make_algebra.
+      - exact make_coequifier_path_algebra.
+      - exact make_coequifier_is_algebra.
+    Defined.
+  End MakeCoequifierAlgebra.
+
+  Section CoequifierMapProjections.
+    Context {X Y : hit_algebra_one_types coequifier_signature}
+            (φ : X --> Y).
+
+    Definition coequifier_map
+      : alg_carrier X → alg_carrier Y
+      := pr111 φ.
+
+    Definition coequifier_map_inc
+               (b : B)
+      : coequifier_map (coequif_inc X b) = coequif_inc Y b
+      := pr1 (pr211 φ) b.
+  End CoequifierMapProjections.
+
+  Section MakeCoequifierMap.
+    Context {X Y : hit_algebra_one_types coequifier_signature}
+            (φ : alg_carrier X → alg_carrier Y)
+            (φ_inc : ∏ (b : B), φ (coequif_inc X b) = coequif_inc Y b).
+
+    Definition make_coequifier_map
+      : X --> Y.
+    Proof.
+      use make_algebra_map.
+      use make_hit_path_alg_map.
+      - use make_hit_prealgebra_mor.
+        + exact φ.
+        + exact φ_inc.
+      - intro i ; induction i.
+    Defined.
+  End MakeCoequifierMap.
+
+  Section CoequifierCellProjections.
+    Context {X Y : hit_algebra_one_types coequifier_signature}
+            {φ ψ : X --> Y}
+            (α : φ ==> ψ).
+
+    Definition coequifier_cell
+      : coequifier_map φ ~ coequifier_map ψ
+      := pr111 α.
+
+    Definition coequifier_cell_inc
+               (b : B)
+      : coequifier_cell (coequif_inc X b) @ coequifier_map_inc ψ b
+        =
+        coequifier_map_inc φ b.
+    Proof.
+      exact (eqtohomot (pr211 α) _ @ pathscomp0rid _).
+    Qed.
+  End CoequifierCellProjections.
+
+  Section MakeCoequifierCell.
+    Context {X Y : hit_algebra_one_types coequifier_signature}
+            {φ ψ : X --> Y}
+            (α : coequifier_map φ ~ coequifier_map ψ)
+            (α_inc : ∏ (b : B),
+                     α (coequif_inc X b) @ coequifier_map_inc ψ b
+                     =
+                     coequifier_map_inc φ b).
+
+    Definition make_coequifier_cell
+      : φ ==> ψ.
+    Proof.
+      simple refine (((α ,, _) ,, (λ _, tt)) ,, tt).
+      abstract
+        (use funextsec ; intro b ;
+         exact (α_inc _ @ !(pathscomp0rid _))).
+    Defined.
+  End MakeCoequifierCell.
+
+  (** The induction principle *)
   Section CoequifierInduction.
     Context {X : hit_algebra_one_types coequifier_signature}
             (Y : alg_carrier X → one_type)
-            (Yinc : ∏ (b : B), Y (coequif_inc b))
+            (Yinc : ∏ (b : B), Y (coequif_inc _ b))
             (Yhomot : ∏ (x : A),
                       globe_over
                         Y
-                        (coequif_homot x)
+                        (coequif_homot _ x)
                         (apd_depfun
                            (λ z, B)
                            Y
@@ -426,8 +528,158 @@ Section CoequifierSignature.
 
     Definition coequif_ind_base
                (b : B)
-      : coequif_ind (coequif_inc b) = Yinc b
+      : coequif_ind (coequif_inc _ b) = Yinc b
       := pr12 coequif_ind_disp_algebra_map b.
   End CoequifierInduction.
+
+  (** Initiality of coequifier *)
+  Definition coequifier_one_types_algebra
+    : hit_algebra_one_types coequifier_signature
+    := pr1 (hit_existence coequifier_signature).
+
+  Definition coequifier_one_types_is_initial
+    : is_initial _ coequifier_one_types_algebra.
+  Proof.
+    apply HIT_is_initial.
+    exact (pr2 (hit_existence coequifier_signature)).
+  Defined.
+
+  Definition coequifier_one_types
+    : one_types
+    := hit_carrier coequifier_signature.
+
+  Definition coequifier_one_types_inc
+    : one_types ⟦ B , coequifier_one_types ⟧
+    := coequif_inc coequifier_one_types_algebra.
+
+  Definition coequifier_one_types_homot
+    : p ▹ coequifier_one_types_inc = q ▹ coequifier_one_types_inc.
+  Proof.
+    use funextsec.
+    exact (λ x, coequif_homot coequifier_one_types_algebra x).
+  Qed.
+
+  Section CoequfierUMPMap.
+    Variable (Y : one_types)
+             (Yinc : one_types ⟦ B , Y ⟧)
+             (Yhomot : p ▹ Yinc = q ▹ Yinc).
+
+    Local Definition coequifier_ump_1_Y_alg
+      : hit_algebra_one_types coequifier_signature.
+    Proof.
+      exact (make_coequifier_algebra Yinc (λ x, eqtohomot Yhomot x)).
+    Defined.
+
+    Local Definition coequifier_ump_1_alg_map
+      : coequifier_one_types_algebra
+        -->
+        coequifier_ump_1_Y_alg.
+    Proof.
+      exact (biinitial_1cell
+               _
+               coequifier_one_types_is_initial
+               coequifier_ump_1_Y_alg).
+    Defined.
+
+    Definition coequifier_ump_1
+      : coequifier_one_types --> Y
+      := coequifier_map coequifier_ump_1_alg_map.
+
+    Definition coequifier_ump_1_inc
+      : coequifier_one_types_inc · coequifier_ump_1
+        ==>
+        Yinc
+      := coequifier_map_inc coequifier_ump_1_alg_map.
+  End CoequfierUMPMap.
+
+  Section CoequifierUMPCell.
+    Context {Y : one_types}
+            {Yinc : one_types ⟦ B , Y ⟧}
+            {Yhomot : p ▹ Yinc = q ▹ Yinc}
+            (φ ψ : coequifier_one_types --> Y)
+            (φinc : coequifier_one_types_inc · φ ==> Yinc)
+            (ψinc : coequifier_one_types_inc · ψ ==> Yinc).
+    
+    Local Definition coequifier_ump_2_alg_cell
+      : @make_coequifier_map
+          coequifier_one_types_algebra
+          (make_coequifier_algebra Yinc (λ x, eqtohomot Yhomot x))
+          φ
+          φinc
+        ==>
+        @make_coequifier_map
+           coequifier_one_types_algebra
+           (make_coequifier_algebra Yinc (λ x, eqtohomot Yhomot x))
+           ψ
+           ψinc
+      := biinitial_2cell
+           _
+           coequifier_one_types_is_initial
+           _ _.
+
+    Definition coequifier_ump_2
+      : φ ==> ψ
+      := coequifier_cell coequifier_ump_2_alg_cell.
+
+    Definition coequifier_ump_2_inc
+      : (coequifier_one_types_inc ◃ coequifier_ump_2) • ψinc
+        =
+        φinc.
+    Proof.
+      use funextsec ; intro b.
+      exact (coequifier_cell_inc coequifier_ump_2_alg_cell b).
+    Qed.
+  End CoequifierUMPCell.
+
+  Section CoequifierUMPEq.
+    Context {Y : one_types}
+            {Yinc : one_types ⟦ B , Y ⟧}
+            {Yhomot : p ▹ Yinc = q ▹ Yinc}
+            {φ ψ : coequifier_one_types --> Y}
+            {φinc : coequifier_one_types_inc · φ ==> Yinc}
+            {ψinc : coequifier_one_types_inc · ψ ==> Yinc}
+            (ρ σ : φ ==> ψ)
+            (ρinc : (coequifier_one_types_inc ◃ ρ) • ψinc = φinc)
+            (σinc : (coequifier_one_types_inc ◃ σ) • ψinc = φinc).
+
+    Definition coequifier_ump_eq
+      : ρ = σ.
+    Proof.
+      pose
+        (@biinitial_eq
+           _ _
+           _ coequifier_one_types_is_initial
+           _
+           _ _
+           (@make_coequifier_cell
+              _ _
+              (@make_coequifier_map
+                 coequifier_one_types_algebra
+                 (make_coequifier_algebra Yinc (λ x, eqtohomot Yhomot x))
+                 φ
+                 φinc)
+              (@make_coequifier_map
+                 coequifier_one_types_algebra
+                 (make_coequifier_algebra Yinc (λ x, eqtohomot Yhomot x))
+                 ψ
+                 ψinc)
+              ρ (eqtohomot ρinc))
+           (@make_coequifier_cell
+              _ _
+              (@make_coequifier_map
+                 coequifier_one_types_algebra
+                 (make_coequifier_algebra Yinc (λ x, eqtohomot Yhomot x))
+                 φ
+                 φinc)
+              (@make_coequifier_map
+                 coequifier_one_types_algebra
+                 (make_coequifier_algebra Yinc (λ x, eqtohomot Yhomot x))
+                 ψ
+                 ψinc)
+              σ (eqtohomot σinc)))
+        as w.
+      exact (maponpaths (λ z, pr111 z) w).
+    Qed.
+  End CoequifierUMPEq.
 End CoequifierSignature.
     
