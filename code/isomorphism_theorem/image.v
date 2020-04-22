dfe23559a348ec9c629421bf0994e5cdef9465f6
Niels <nnmvdw@gmail.com>
2020-04-22 15:50:22 +0200

    New files

---
+++ code/isomorphism_theorem/image.v
@@ -0,0 +1,33 @@
+(** Image of algebras *)
+Require Import UniMath.Foundations.All.
+Require Import UniMath.MoreFoundations.All.
+
+Require Import UniMath.CategoryTheory.Core.Categories.
+Require Import UniMath.Bicategories.Core.Bicat.
+Import Bicat.Notations.
+Require Import UniMath.Bicategories.Core.Examples.OneTypes.
+Require Import UniMath.Bicategories.DisplayedBicats.DispBicat.
+Require Import UniMath.Bicategories.DisplayedBicats.Examples.Algebras.
+Require Import UniMath.Bicategories.DisplayedBicats.Examples.DispDepProd.
+Require Import UniMath.Bicategories.DisplayedBicats.Examples.Add2Cell.
+Require Import UniMath.Bicategories.Colimits.Initial.
+
+Require Import UniMath.Bicategories.PseudoFunctors.Display.Base.
+Require Import UniMath.Bicategories.PseudoFunctors.Display.Map1Cells.
+Require Import UniMath.Bicategories.PseudoFunctors.Display.Map2Cells.
+Require Import UniMath.Bicategories.PseudoFunctors.Display.Identitor.
+Require Import UniMath.Bicategories.PseudoFunctors.Display.Compositor.
+Require Import UniMath.Bicategories.PseudoFunctors.Display.PseudoFunctorBicat.
+Require Import UniMath.Bicategories.PseudoFunctors.PseudoFunctor.
+
+Require Import prelude.all.
+Require Import signature.hit_signature.
+Require Import signature.hit.
+Require Import algebra.one_types_polynomials.
+Require Import algebra.one_types_endpoints.
+Require Import algebra.one_types_homotopies.
+Require Import displayed_algebras.globe_over_lem.
+Require Import displayed_algebras.displayed_algebra.
+Require Import displayed_algebras.total_algebra.
+
+Local Open Scope cat.
