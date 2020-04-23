a1bff4bf17a26402eb59469fd0e3191de6a9399d
Niels <nnmvdw@gmail.com>
2020-04-23 15:31:44 +0200

    Further WIP

--- code/isomorphism_theorem/congruence.v
+++ code/isomorphism_theorem/congruence.v
@@ -76,16 +76,112 @@ Section CongruenceRelation.
 
   Section MakeGroupoidAlgebraFromCongruence.
     Variable (R : congruence_relation).
+
+    Definition make_groupoid_algebra_carrier_precategory_data
+      : precategory_data.
+    Proof.
+      use make_precategory_data.
+      - use make_precategory_ob_mor.
+        + exact (alg_carrier X).
+        + apply TODO.
+      - apply TODO.
+      - apply TODO.
+    Defined.
+
+    Definition make_groupoid_algebra_carrier_is_precategory
+      : is_precategory make_groupoid_algebra_carrier_precategory_data.
+    Proof.
+      use make_is_precategory.
+      - apply TODO.
+      - apply TODO.
+      - apply TODO.
+      - apply TODO.
+    Qed.
     
-    Definition make_groupoid_algebra_carrier
-      : groupoid.
+    Definition make_groupoid_algebra_carrier_precategory
+      : precategory.
+    Proof.
+      use make_precategory.
+      - exact make_groupoid_algebra_carrier_precategory_data.
+      - exact make_groupoid_algebra_carrier_is_precategory.
+    Defined.
+
+    Definition make_groupoid_algebra_carrier_has_homsets
+      : has_homsets make_groupoid_algebra_carrier_precategory.
+    Proof.
+      apply TODO.
+    Qed.
+    
+    Definition make_groupoid_algebra_carrier_category
+      : category.
+    Proof.
+      use make_category.
+      - exact make_groupoid_algebra_carrier_precategory.
+      - exact make_groupoid_algebra_carrier_has_homsets.
+    Defined.
+
+    Definition make_groupoid_algebra_carrier_is_pregroupoid
+      : is_pregroupoid make_groupoid_algebra_carrier_category.
     Proof.
       apply TODO.
     Defined.
     
+    Definition make_groupoid_algebra_carrier
+      : groupoid.
+    Proof.
+      use make_groupoid.
+      - exact make_groupoid_algebra_carrier_category.
+      - exact make_groupoid_algebra_carrier_is_pregroupoid.
+    Defined.
+
+    Definition make_groupoid_algebra_operations_functor_data
+      : functor_data
+          (⦃ point_constr Σ ⦄ make_groupoid_algebra_carrier : groupoid)
+          make_groupoid_algebra_carrier.
+    Proof.
+      use make_functor_data.
+      - exact (alg_constr X).
+      - apply TODO.
+    Defined.
+
+    Definition make_groupoid_algebra_operations_is_functor
+      : is_functor make_groupoid_algebra_operations_functor_data.
+    Proof.
+      split.
+      - apply TODO.
+      - apply TODO.
+    Qed.
+    
+    Definition make_groupoid_algebra_operations
+      : (⦃ point_constr Σ ⦄ make_groupoid_algebra_carrier : groupoid)
+        ⟶
+        make_groupoid_algebra_carrier.
+    Proof.
+      use make_functor.
+      - exact make_groupoid_algebra_operations_functor_data.
+      - exact make_groupoid_algebra_operations_is_functor.
+    Defined.
+
+    Definition make_groupoid_prealgebra
+      : hit_prealgebra_grpd Σ.
+    Proof.
+      use make_hit_prealgebra_grpd.
+      - exact make_groupoid_algebra_carrier.
+      - exact make_groupoid_algebra_operations.
+    Defined.
+
+    Definition make_groupoid_path_algebra
+      : hit_path_algebra_grpd Σ.
+    Proof.
+      use make_hit_path_algebra_grpd.
+      - exact make_groupoid_prealgebra.
+      - simpl.
+    
     Definition make_groupoid_algebra
       : hit_algebra_grpd Σ.
     Proof.
+      use make_algebra_grpd.
+      - 
       apply TODO.
     Defined.
   End MakeGroupoidAlgebraFromCongruence.
