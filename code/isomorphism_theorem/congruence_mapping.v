425ee4a1360df673cad5c4a192f03050c88d9f0b
Niels <nnmvdw@gmail.com>
2020-04-24 22:46:56 +0200

    Finished mapping principle

--- code/isomorphism_theorem/congruence_mapping.v
+++ code/isomorphism_theorem/congruence_mapping.v
@@ -42,6 +42,7 @@ Require Import algebra.groupoid_homotopies.
 Require Import hit_biadjunction.path_groupoid_commute.
 Require Import hit_biadjunction.hit_algebra_biadj.lift_path_groupoid.
 Require Import hit_biadjunction.hit_path_algebra_biadj.lift_path_groupoid.
+Require Import hit_biadjunction.hit_prealgebra_biadj.lift_path_groupoid.
 Require Import hit_biadjunction.hit_algebra_biadj.lift_path_groupoid.
 Require Import hit_biadjunction.hit_algebra_biadj.
 Require Import existence.initial_algebra.
@@ -49,10 +50,14 @@ Require Import isomorphism_theorem.congruence_relation.
 
 Local Open Scope cat.
 
-Definition TODO {A : UU} : A.
-Admitted.
-
-
+Definition idtoiso_path_groupoid
+           {X : one_type}
+           {x y : X}
+           (p : x = y)
+  : pr1 (@idtoiso (one_type_to_groupoid X) _ _ p) = p.
+Proof.
+  induction p ; apply idpath.
+Defined.
 
 Definition poly_rel_to_eq_fun
            {P : poly_code}
@@ -107,7 +112,28 @@ Section MapToQuotientFromCongruence.
                        (g₂ : cong_rel_carrier R a₂ a₃),
                      fR a₁ a₃ (cong_rel_comp R g₁ g₂)
                      =
-                     fR a₁ a₂ g₁ @ fR a₂ a₃ g₂).
+                     fR a₁ a₂ g₁ @ fR a₂ a₃ g₂)
+           (fRcommute : ∏ (x y : poly_act
+                                   (point_constr Σ)
+                                   (alg_carrier X))
+                          (p : poly_act_rel
+                                 (point_constr Σ)
+                                 (cong_rel_carrier R)
+                                 x y),
+                        (fR (alg_constr X x) (alg_constr X y) (cong_rel_ops R x y p)
+                         @ alg_map_commute f y
+                         =
+                         alg_map_commute f x
+                         @ maponpaths (alg_constr Y) (poly_rel_to_eq_fun fR p)))
+           (fRpath : ∏ (j : path_label Σ)
+                       (x : poly_act (path_source Σ j) (alg_carrier X)),
+                     sem_endpoint_UU_natural (path_left Σ j) (alg_map_commute f) x
+                     @ alg_path Y j (poly_map (path_source Σ j) (alg_map_carrier f) x)
+                     =
+                     fR (sem_endpoint_UU (path_left Σ j) (alg_constr X) x)
+                        (sem_endpoint_UU (path_right Σ j) (alg_constr X) x)
+                        (make_groupoid_path_algebra_nat_trans_data R j x)
+                     @ sem_endpoint_UU_natural (path_right Σ j) (alg_map_commute f) x).
   
   Definition factor_through_gquot_grpd_data
     : functor_data
@@ -151,17 +177,59 @@ Section MapToQuotientFromCongruence.
     apply poly_path_groupoid_is_id.
   Defined.
 
-  Definition yolo
+  Definition factor_through_gquot_grpd_op_is_nat_trans_lem
              {P : poly_code}
              {x y : poly_act P (alg_carrier X)}
-             (p : poly_act_rel P (R) x y)
-    : UU.
-             poly_rel_to_eq_fun fR p @
-  ! poly_path_groupoid_is_id (poly_map (point_constr Σ) (alg_map_carrier f) y) =
-  ! poly_path_groupoid_is_id (poly_map (point_constr Σ) (alg_map_carrier f) x) @
-  # (poly_path_groupoid (point_constr Σ) (pr111 Y))
-    (poly_act_functor_morphisms (point_constr Σ) factor_through_gquot_grpd_carrier p)
-
+             (p : poly_act_rel P (cong_rel_carrier R) x y)
+    : poly_rel_to_eq_fun fR p
+      @ !(poly_path_groupoid_is_id (poly_map P (alg_map_carrier f) y))
+      =
+      !(poly_path_groupoid_is_id (poly_map P (alg_map_carrier f) x))
+      @ # (poly_path_groupoid P (pr111 Y) : _ ⟶ _)
+          (poly_act_functor_morphisms P factor_through_gquot_grpd_carrier p).
+  Proof.
+    simpl.
+    induction P as [ T | | P₁ IHP₁ P₂ IHP₂ | P₁ IHP₁ P₂ IHP₂ ].
+    - apply pathscomp0rid.
+    - apply pathscomp0rid.
+    - induction x as [x | x], y as [y | y].
+      + simpl.
+        etrans.
+        {
+          apply maponpaths.
+          exact (!(maponpathsinv0 _ _)).
+        }
+        refine (!(maponpathscomp0 _ _ _) @ _).
+        refine (maponpaths (maponpaths inl) (IHP₁ _ _ _) @ _).
+        refine (maponpathscomp0 _ _ _ @ _).
+        apply maponpaths_2.
+        apply maponpathsinv0.
+      + induction p.
+      + induction p.
+      + simpl.
+        etrans.
+        {
+          apply maponpaths.
+          exact (!(maponpathsinv0 _ _)).
+        }
+        refine (!(maponpathscomp0 _ _ _) @ _).
+        refine (maponpaths (maponpaths inr) (IHP₂ _ _ _) @ _).
+        refine (maponpathscomp0 _ _ _ @ _).
+        apply maponpaths_2.
+        apply maponpathsinv0.
+    - simpl.
+      etrans.
+      {
+        apply maponpaths.
+        apply pathsdirprod_inv.
+      }
+      refine (pathsdirprod_concat _ _ _ _ @ _).
+      refine (paths_pathsdirprod (IHP₁ _ _ _) (IHP₂ _ _ _) @ _).
+      refine (!(pathsdirprod_concat _ _ _ _) @ _).
+      apply maponpaths_2.
+      refine (!_).
+      apply pathsdirprod_inv.
+  Qed.        
   
   Definition factor_through_gquot_grpd_op_is_nat_trans
     : is_nat_trans _ _ factor_through_gquot_grpd_op_data.
@@ -174,33 +242,18 @@ Section MapToQuotientFromCongruence.
       apply maponpaths.
       exact (!(maponpathscomp0 (alg_constr Y) _ _)).
     }
-    cbn in p.
-    unfold poly_act_morphism in p.
-    unfold precategory_morphisms in p.
-    pose (q := p : poly_act_rel (point_constr Σ) (cong_rel_carrier R) x y).
-
-    assert (fR (alg_constr X x) (alg_constr X y) (cong_rel_ops R x y p)
-            @ alg_map_commute f y
-            =
-            alg_map_commute f x
-            @ maponpaths (alg_constr Y) (poly_rel_to_eq_fun fR p))
-      as H.
-    {
-      apply TODO.
-    }
     refine (!_).
     etrans.
     {
       refine (path_assoc _ _ _ @ _).
       apply maponpaths_2.
-      exact H.
+      exact (fRcommute _ _ p).
     }
     refine (!(path_assoc _ _ _) @ _).
     apply maponpaths.
     refine (!(maponpathscomp0 (alg_constr Y) _ _) @ _).
     apply maponpaths.
-    simpl in x, y.
-    apply TODO.
+    exact (factor_through_gquot_grpd_op_is_nat_trans_lem p).
   Qed.
       
   Definition factor_through_gquot_grpd_op
@@ -225,6 +278,193 @@ Section MapToQuotientFromCongruence.
     - exact factor_through_gquot_grpd_op.
   Defined.
 
+  Definition factor_through_gquot_grod_preserves_path_help
+             {P Q : poly_code}
+             (e : endpoint (point_constr Σ) P Q)
+             (x : poly_act P (alg_carrier X))
+    : poly_act_compose
+        (sem_endpoint_grpd_natural_data
+           e
+           factor_through_gquot_grpd_prealg
+           x)
+        (poly_act_morphism_to_path
+           (path_groupoid_endpoint
+              e
+              (poly_map P (alg_map_carrier f) x)))
+      =
+      @poly_act_morphism_to_path
+        Q
+        (pr111 (hit_algebra_biadjunction Σ Y))
+        _ _
+        (sem_endpoint_UU_natural
+           e
+           (alg_map_commute f)
+           x).
+  Proof.
+    simpl.
+    induction e as [P | P Q Q' e₁ IHe₁ e₂ IHe₂
+                    | P Q | P Q | P Q | P Q
+                    | P Q Q' e₁ IHe₁ e₂ IHe₂
+                    | P T t | Z₁ Z₂ g | ].
+    - apply poly_act_id_left.
+    - simpl.
+      refine (_ @ poly_act_compose_poly_act_morphism_to_path _ _).
+      refine (!_).
+      etrans.
+      {
+        apply maponpaths_2.
+        exact (!(IHe₂ _)).
+      }
+      clear IHe₂.
+      refine (!(poly_act_assoc _ _ _) @ _).
+      refine (_ @ poly_act_assoc _ _ _).
+      apply maponpaths.
+      refine (poly_act_compose_poly_act_morphism_to_path _ _ @ _).
+      etrans.
+      {
+        apply maponpaths.
+        refine (!_).
+        apply homotsec_natural'.
+      }
+      refine (!(poly_act_compose_poly_act_morphism_to_path _ _) @ _).
+      refine (!_).
+      etrans.
+      {
+        do 2 apply maponpaths.
+        refine (!_).
+        apply homotsec_natural'.
+      }
+      etrans.
+      {
+        apply maponpaths.
+        refine (!_).
+        apply poly_act_compose_poly_act_morphism_to_path.
+      }
+      refine (poly_act_assoc _ _ _ @ _).
+      apply maponpaths_2.
+      etrans.
+      {
+        apply maponpaths.
+        refine (!_).
+        apply poly_path_groupoid_poly_act_morphism_to_path.
+      }
+      refine (_ @ poly_path_groupoid_poly_act_morphism_to_path _ _ _).
+      etrans.
+      {
+        refine (!_).
+        apply (functor_comp
+                 (sem_endpoint_grpd
+                    e₂
+                    (_ ,, prealg_path_groupoid_map (point_constr Σ) (pr111 Y) (pr211 Y)))).
+      }
+      apply maponpaths.
+      apply IHe₁.
+    - apply poly_act_id_left.
+    - apply poly_act_id_left.
+    - apply poly_act_id_left.
+    - apply poly_act_id_left.
+    - simpl.
+      apply pathsdirprod.
+      + refine (_ @ IHe₁ _ @ _).
+        * do 2 apply maponpaths.
+          apply maponpaths_pr1_pathsdirprod.
+        * apply maponpaths.
+          refine (!_).
+          apply maponpaths_pr1_pathsdirprod.
+      + refine (_ @ IHe₂ _ @ _).
+        * do 2 apply maponpaths.
+          apply maponpaths_pr2_pathsdirprod.
+        * apply maponpaths.
+          refine (!_).
+          apply maponpaths_pr2_pathsdirprod.
+    - apply idpath.
+    - apply idpath.
+    - simpl ; unfold factor_through_gquot_grpd_op_data.
+      refine (_ @ !(idtoiso_path_groupoid _)).
+      etrans.
+      {
+        apply maponpaths.
+        apply idtoiso_path_groupoid.
+      }
+      refine (!(path_assoc _ _ _) @ _).
+      etrans.
+      {
+        apply maponpaths.
+        etrans.
+        {
+          apply maponpaths_2.
+          apply maponpathsinv0.
+        }
+        apply pathsinv0l.
+      }
+      apply pathscomp0rid.
+  Qed.
+
+  Definition factor_through_gquot_grod_preserves_path_help'
+             {P : poly_code}
+             (e : endpoint (point_constr Σ) P I)
+             (x : poly_act P (alg_carrier X))
+    : sem_endpoint_grpd_natural_data
+        e
+        factor_through_gquot_grpd_prealg
+        x
+      @ path_groupoid_endpoint
+          e
+          (poly_map P (alg_map_carrier f) x)
+      =
+      sem_endpoint_UU_natural
+        e
+        (alg_map_commute f)
+        x.
+  Proof.
+    pose (factor_through_gquot_grod_preserves_path_help e x) as p.
+    refine (_ @ p @ idtoiso_path_groupoid _).
+    apply maponpaths.
+    refine (!_).
+    apply idtoiso_path_groupoid.
+  Qed.
+  
+  Definition factor_through_gquot_grod_preserves_path
+             (j : path_label Σ)
+             (x : poly_act (path_source Σ j) (alg_carrier X))
+    : fR
+        (sem_endpoint_UU (path_left Σ j) (alg_constr X) x)
+        (sem_endpoint_UU (path_right Σ j) (alg_constr X) x)
+        (make_groupoid_path_algebra_nat_trans_data R j x)
+      @ sem_endpoint_grpd_natural_data
+          (path_right Σ j)
+          factor_through_gquot_grpd_prealg
+          x
+      =
+      sem_endpoint_grpd_natural_data
+        (path_left Σ j)
+        factor_through_gquot_grpd_prealg
+        x
+      @ path_groupoid_endpoint
+          (path_left Σ j)
+          (poly_map (path_source Σ j) (alg_map_carrier f) x)
+      @ alg_path Y j (poly_map (path_source Σ j) (alg_map_carrier f) x)
+      @ !(path_groupoid_endpoint
+            (path_right Σ j)
+            (poly_map (path_source Σ j) (alg_map_carrier f) x)).
+  Proof.
+    do 2 refine (_ @ !(path_assoc _ _ _)).
+    use path_inv_rotate_rr.
+    refine (!(path_assoc _ _ _) @ _).
+    etrans.
+    {
+      apply maponpaths.
+      exact (factor_through_gquot_grod_preserves_path_help' (path_right Σ j) x).
+    }
+    refine (!_).
+    etrans.
+    {
+      apply maponpaths_2.
+      exact (factor_through_gquot_grod_preserves_path_help' (path_left Σ j) x).
+    }
+    exact (fRpath j x).
+  Qed.
+
   Definition factor_through_gquot_grpd
     : make_groupoid_algebra R
       -->
@@ -233,7 +473,7 @@ Section MapToQuotientFromCongruence.
     use make_algebra_map_grpd.
     use make_hit_path_alg_map_grpd.
     - exact factor_through_gquot_grpd_prealg.
-    - apply TODO.
+    - exact factor_through_gquot_grod_preserves_path.
   Defined.
 
   Definition factor_through_gquot
@@ -242,4 +482,39 @@ Section MapToQuotientFromCongruence.
     use quotient_of_congruence_map.
     exact factor_through_gquot_grpd.
   Defined.
+
+  Definition factor_through_gquot_gcl
+             (x : alg_carrier X)
+    : alg_map_carrier
+        factor_through_gquot
+        (gcl (make_groupoid_algebra_carrier R) x)
+      =
+      alg_map_carrier f x.
+  Proof.
+    apply idpath.
+  Defined.
+
+  Definition factor_through_gquot_gcleq
+             {x y : alg_carrier X}
+             (p : cong_rel_carrier R x y)
+    : maponpaths
+        (alg_map_carrier factor_through_gquot)
+        (gcleq
+           (make_groupoid_algebra_carrier R)
+           p)
+      =
+      fR _ _ p.
+  Proof.
+    refine (!(maponpathscomp
+                (gquot_functor_map factor_through_gquot_grpd_carrier)
+                (gquot_counit_map (pr111 Y))
+                (gcleq (make_groupoid_algebra_carrier R) p))
+             @ _).
+    etrans.
+    {
+      refine (maponpaths (maponpaths (gquot_counit_map (pr111 Y))) _).
+      exact (gquot_rec_beta_gcleq (make_groupoid_algebra_carrier R) _ _ _ _ _ _ _ _ p).
+    }
+    exact (gquot_rec_beta_gcleq _ _ _ _ _ _ _ _ _ _).
+  Qed.
 End MapToQuotientFromCongruence.
