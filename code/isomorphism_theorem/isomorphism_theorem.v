d860daf261b1ba4d7195c550009afef7d6fdac90
Niels <nnmvdw@gmail.com>
2020-04-25 17:12:32 +0200

    Work on isomorphism theorem

--- code/isomorphism_theorem/isomorphism_theorem.v
+++ code/isomorphism_theorem/isomorphism_theorem.v
@@ -41,6 +41,7 @@ Require Import algebra.one_types_polynomials.
 Require Import algebra.one_types_endpoints.
 Require Import algebra.one_types_homotopies.
 Require Import algebra.one_types_adjoint_equivalence.
+Require Import displayed_algebras.globe_over_lem.
 Require Import existence.initial_algebra.
 Require Import isomorphism_theorem.congruence_relation.
 Require Import isomorphism_theorem.congruence_mapping.
@@ -68,7 +69,7 @@ Section FirstIsomorphismTheorem.
     - apply TODO.
     - apply TODO.
     - apply TODO.
-    - apply TODO.
+    - exact (λ _ _ _ p q, p @ q).
     - apply TODO.
     - apply TODO.
     - apply TODO.
@@ -113,7 +114,12 @@ Section FirstIsomorphismTheorem.
   Proof.
     use factor_through_gquot.
     - exact (image_inj f).
-    - apply TODO.
+    - simpl ; cbn.
+      unfold image_inj_comp.
+      intros a b p.
+      use PathOverToTotalPath.
+      + exact p.
+      + apply TODO.
     - apply TODO.
     - apply TODO.
     - apply TODO.
@@ -123,6 +129,128 @@ Section FirstIsomorphismTheorem.
   (**
      Step 3: Show this map is a left adjoint equivalence
    *)
+  Section FiberMapGQuot.
+    Variable (x : alg_carrier X).
+
+    Definition inverse_image_map_gquot
+      : hfiber
+          (alg_map_carrier map_gquot_to_image)
+          (alg_map_carrier f x,, hinhpr (x,, idpath (alg_map_carrier f x)))
+      := gcl (make_groupoid_algebra_carrier congruence_relation_for_image) x
+         ,,
+         idpath _.
+
+    Definition inverse_image_map_gquot_all_path_base
+      : ∏ (z : alg_carrier X/R),
+        alg_map_carrier
+          map_gquot_to_image
+          z
+        =
+        alg_map_carrier f x
+        ,,
+        hinhpr (x,, idpath (alg_map_carrier f x))
+        →
+        z
+        =
+        gcl (make_groupoid_algebra_carrier congruence_relation_for_image) x.
+    Proof.
+      use gquot_ind_set ; simpl.
+      - exact (λ a p,
+               gcleq
+                 (make_groupoid_algebra_carrier congruence_relation_groupoid_for_image)
+                 (maponpaths pr1 p)).
+      - abstract
+          (simpl ;
+           intros a₁ a₂ g ;
+           apply PathOver_arrow ;
+           intro p ;
+           apply map_PathOver ;
+           unfold square ;
+           rewrite maponpathsidfun ;
+           rewrite maponpaths_for_constant_function ;
+           rewrite pathscomp0rid ;
+           refine (!(gconcat _ _ _) @ _) ;
+           apply maponpaths ;
+           unfold transportb ;
+           rewrite transportf_paths_FlFr ;
+           rewrite maponpaths_for_constant_function ;
+           rewrite pathscomp0rid ;
+           rewrite maponpathsinv0 ;
+           rewrite pathsinv0inv0 ;
+           rewrite maponpathscomp0 ;
+           refine (!_) ;
+           apply maponpaths_2 ;
+           refine (maponpaths
+                     (maponpaths pr1)
+                     (factor_through_gquot_gcleq
+                        congruence_relation_for_image
+                        (image f)
+                        _ _ _ _ _ _
+                        g)
+                   @ _) ;
+           apply maponpaths_pr1_PathOverToTotalPath).
+      - intro.
+        use funspace_isaset.
+        exact (gtrunc _ _ _).
+    Defined.
+
+    Definition inverse_image_map_gquot_all_path_fib
+      : ∏ (z : alg_carrier X/R)
+          (p : alg_map_carrier map_gquot_to_image z
+               =
+               alg_map_carrier f x,, hinhpr (x,, idpath (alg_map_carrier f x))),
+        maponpaths
+          (alg_map_carrier map_gquot_to_image)
+          (inverse_image_map_gquot_all_path_base z p)
+        =
+        p.
+    Proof.
+      use gquot_ind_prop.
+      - intros a p.
+        simpl.
+        etrans.
+        {
+          exact (factor_through_gquot_gcleq
+                   congruence_relation_for_image
+                   (image f)
+                   _ _ _ _ _ _
+                   _).
+        }
+        refine (_ @ !(PathOverToTotalPath'_eta _)).
+        unfold PathOverToTotalPath'.
+        use globe_over_to_homot.
+        * apply idpath.
+        * use path_globe_over_hset.
+          intro.
+          apply isasetaprop.
+          simpl.
+          exact (pr2 (ishinh _)).
+      - intro ; simpl.
+        use impred_isaprop ; intro ; simpl.
+        exact (one_type_isofhlevel _ _ _ _ _).
+    Qed.
+        
+    Definition inverse_image_map_gquot_all_path
+               (t : hfiber
+                      (alg_map_carrier map_gquot_to_image)
+                      (alg_map_carrier f x,, hinhpr (x,, idpath (alg_map_carrier f x))))
+      : t = inverse_image_map_gquot.
+    Proof.
+      induction t as [z p].
+      use PathOverToTotalPath.
+      - exact (inverse_image_map_gquot_all_path_base z p).
+      - abstract
+          (use map_PathOver ;
+           unfold square ;
+           refine (pathscomp0rid _ @ _) ;
+           refine (!_) ;
+           refine (maponpaths (λ z, _ @ z) (maponpaths_for_constant_function _ _) @ _) ;
+           refine (pathscomp0rid _ @ _) ;
+           refine (!_) ;
+           exact (inverse_image_map_gquot_all_path_fib z p)).
+    Qed.
+  End FiberMapGQuot.
+  
   Definition map_gquot_to_image_is_adjequiv
     : left_adjoint_equivalence map_gquot_to_image.
   Proof.
@@ -140,9 +268,7 @@ Section FirstIsomorphismTheorem.
       induction x as [x p].
       induction p ; simpl.
       use make_iscontr.
-      + exact (gcl (make_groupoid_algebra_carrier _) x ,, idpath _).
-      + intro t.
-        induction t as [z p].
-        apply TODO.
+      + exact (inverse_image_map_gquot x).
+      + exact (inverse_image_map_gquot_all_path x).
   Defined.
 End FirstIsomorphismTheorem.
